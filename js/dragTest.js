eb34111f9cf5009948b812561b0112021bee1778
FredKF <fedetkd86@hotmail.com>
2021-03-13 15:19:19 +0100

    drag test

--- js/dragTest.js
+++ js/dragTest.js
@@ -2,16 +2,16 @@ const quantityElement = document.getElementById("quantity");
 let quantity = 0;
 
 function allowDrop(ev){
-    ev.preventDefault();
+    ev.preventDefault();    
 }
 
 function drag(ev){
-    ev.dataTransfer.setData("text", ev.target.id);
+    ev.dataTransfer.setData("data", ev.target.id);
 }
 
 function drop(ev){
     ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));    
     quantityElement.innerHTML = quantity++;
+    var data = ev.dataTransfer.getData("data"); 
+    ev.target.appendChild(document.getElementById(data));        
 }
